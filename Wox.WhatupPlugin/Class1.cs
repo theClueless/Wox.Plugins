350af24f3cc29b64d73c64ec78d1b5655c25815d
Amir Tepper <amir.tepper@intel.com>
2019-10-17 10:11:56 +0300

    Add project files.

---
+++ Wox.WhatupPlugin/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wox.WhatupPlugin
+{
+    public class Class1
+    {
+    }
+}
