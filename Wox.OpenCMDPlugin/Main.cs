dfd02bc6c82ff27106ffef5e1db5ce1d48b2ef09
AT <amir.tepper@gmail.com>
2019-10-21 00:27:23 +0300

    small fixes

--- Wox.OpenCMDPlugin/Main.cs
+++ Wox.OpenCMDPlugin/Main.cs
@@ -13,8 +13,8 @@ namespace Wox.Plugin.OpenCMD
 {
     public class Main : IPlugin
     {
-        private PluginInitContext context;
-        private static List<SystemWindow> openingWindows = new List<SystemWindow>();
+        private const string ExplorerProcessName = "explorer";
+        private List<SystemWindow> _openingWindows = new List<SystemWindow>();
 
         static Main()
         {
@@ -27,109 +27,111 @@ namespace Wox.Plugin.OpenCMD
         {
             var list = new List<Result>();
 
-            SystemWindow win = null;
-            GetOpeningWindows();
-            if (openingWindows.Count > 0)
+            if (TryGetLastUsedExplorerWindowHandle(out var windowHandle))
             {
-                if (openingWindows[0].Process.ProcessName == "explorer")
+                foreach (var window in GetWindowsWithPaths())
                 {
-                    win = openingWindows[0];
+                    // immediately open the windows command
+                    if (windowHandle == (IntPtr)window.HWND)
+                    {
+                        var path = GetAndVerifyPathFromWindow(window);
+                        if (path == null)
+                            break;
+
+                        StartShell(path);
+                        return list;
+                    }
                 }
             }
 
-            if (win != null)
+            // list all windows explorer paths
+            foreach (var window in GetWindowsWithPaths())
             {
-                foreach (SHDocVw.InternetExplorer window in new SHDocVw.ShellWindows())
+                var path = GetAndVerifyPathFromWindow(window);
+                if (path == null)
+                    continue;
+
+                list.Add(new Result
                 {
-                    var filename = Path.GetFileNameWithoutExtension(window.FullName).ToLower();
-                    if (filename.ToLowerInvariant() == "explorer")
+                    IcoPath = "Images\\app.png",
+                    Title = path,
+                    SubTitle = "Open cmd in this path",
+                    Action = (c) =>
                     {
-                        if (!window.LocationURL.ToLower().Contains("file:"))
-                            continue;
-
-                        // immediately open the windows command
-                        if (win.HWnd == (IntPtr)window.HWND)
-                        {
-                            var path = window.LocationURL.Replace("file:///", "");
-                            StartShell(path);
-                            this.context.API.HideApp();
-                            return list;
-                        }
+                        StartShell(path);
+                        return true;
                     }
-
-                }
-            }
-
-            if (list.Count <= 0)
-            {
-                // list all opening folder
-                foreach (SHDocVw.InternetExplorer window in new SHDocVw.ShellWindows())
-                {
-                    var filename = Path.GetFileNameWithoutExtension(window.FullName).ToLower();
-                    if (filename.ToLowerInvariant() == "explorer")
-                    {
-                        if (!window.LocationURL.ToLower().Contains("file:"))
-                            continue;
-
-                        var path = window.LocationURL.Replace("file:///", "");
-                        path = HttpUtility.UrlDecode(path);
-                        if (!Directory.Exists(path))
-                            continue;
-
-                        list.Add(new Result()
-                        {
-                            IcoPath = "Images\\app.png",
-                            Title = path,
-                            SubTitle = "Open cmd in this path",
-                            Action = (c) =>
-                            {
-                                StartShell(path);
-                                return true;
-                            }
-                        });
-
-                    }
-                }
+                });
             }
 
             return list;
         }
 
+        public IEnumerable<SHDocVw.InternetExplorer> GetWindowsWithPaths()
+        {
+            var windows = new SHDocVw.ShellWindows();
+            foreach (SHDocVw.InternetExplorer shellWindow in windows)
+            {
+                if (IsWindowsExplorerWindowWithPath(shellWindow))
+                {
+                    yield return shellWindow;
+                }
+            }
+        }
+
+        public string GetAndVerifyPathFromWindow(SHDocVw.InternetExplorer window)
+        {
+            var path = window.LocationURL.Replace("file:///", "");
+            path = HttpUtility.UrlDecode(path);
+            if (!Directory.Exists(path))
+                return null;
+
+            return path;
+        }
+
+        public bool IsWindowsExplorerWindowWithPath(SHDocVw.InternetExplorer window)
+        {
+            var filename = Path.GetFileNameWithoutExtension(window.FullName).ToLower();
+            if (filename.ToLowerInvariant() == ExplorerProcessName)
+            {
+                if (!window.LocationURL.ToLower().Contains("file:"))
+                    return false;
+
+                return true;
+            }
+
+            return false;
+        }
+
         public void Init(PluginInitContext context)
         {
-            this.context = context;
         }
 
         private static void StartShell(string path)
         {
-            path = HttpUtility.UrlDecode(path);
-            var cmder = Environment.GetEnvironmentVariable("CMDER_ROOT");
-            if (!string.IsNullOrEmpty(cmder))
+            Process.Start(new ProcessStartInfo()
             {
-                Process.Start(new ProcessStartInfo()
-                {
-                    FileName = Path.Combine(cmder, "cmder.exe"),
-                    Arguments = "/START \"" + path + "\""
-                });
-            }
-            else
-            {
-                Process.Start(new ProcessStartInfo()
-                {
-                    FileName = "cmd",
-                    WorkingDirectory = path
-                });
-            }
+                FileName = "cmd",
+                WorkingDirectory = path
+            });
         }
 
-        private static void GetOpeningWindows()
+        private bool TryGetLastUsedExplorerWindowHandle(out IntPtr windowHandle)
         {
-            openingWindows = new List<SystemWindow>();
+            _openingWindows = new List<SystemWindow>();
             WinApi.EnumWindowsProc callback = EnumWindows;
             WinApi.EnumWindows(callback, 0);
+            if (_openingWindows.Count > 0 && _openingWindows[0].Process.ProcessName == ExplorerProcessName)
+            {
+                windowHandle = _openingWindows[0].HWnd;
+                return true;
+            }
+
+            windowHandle = default;
+            return false;
         }
 
-        private static bool EnumWindows(IntPtr hWnd, int lParam)
+        private bool EnumWindows(IntPtr hWnd, int lParam)
         {
             if (!WinApi.IsWindowVisible(hWnd))
                 return true;
@@ -147,7 +149,7 @@ namespace Wox.Plugin.OpenCMD
                 var window = new SystemWindow(hWnd);
                 if (window.IsAltTabWindow() && !window.IsTopmostWindow())
                 {
-                    openingWindows.Add(window);
+                    _openingWindows.Add(window);
                 }
             }
 
