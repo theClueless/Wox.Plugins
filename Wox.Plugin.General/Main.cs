045beaf5fcf84472a98081d638db368bdb050e3c
AT <amir.tepper@gmail.com>
2019-11-26 23:47:31 +0200

    updated word counter

--- Wox.Plugin.General/Main.cs
+++ Wox.Plugin.General/Main.cs
@@ -28,26 +28,46 @@ namespace Wox.Plugin.General
 
             var queryString = query.Search.Trim();
             
-            if (query.ActionKeyword == CountActionKeyword && queryString == string.Empty)
-            { // try clipboard
-                var clipboard = _clipboardHelper.GetClipboardText();
-                if (!string.IsNullOrEmpty(clipboard))
+            if (query.ActionKeyword == CountActionKeyword)
+            {
+                var stringToCount = queryString;
+                bool fromClip = false;
+                if (string.IsNullOrWhiteSpace(stringToCount))
+                { // try clipboard
+                    stringToCount = _clipboardHelper.GetClipboardText();
+                    fromClip = true;
+                }
+
+                if (!string.IsNullOrEmpty(stringToCount))
                 {
-                    var cut = clipboard.Substring(0, clipboard.Length > 50 ? 50 : clipboard.Length).Replace(Environment.NewLine,"#L");
-                    list.Add(
-                        new Result
-                        {
-                            Score = 99,
-                            IcoPath = "Images\\count.png",
-                            Title = $"Char Count: {clipboard.Length}",
-                            SubTitle = cut
-                        });
+                    var result = CreateCountResultFromString(stringToCount, fromClip);
+                    list.Add(result);
                 }
             }
 
             return list;
         }
-        
+
+        private static Result CreateCountResultFromString(string stringToCount, bool fromClip)
+        {
+            var maxLength = 50;
+            var cut = stringToCount.Substring(0, stringToCount.Length > maxLength ? maxLength : stringToCount.Length).Replace(Environment.NewLine, "#L");
+            var title = $"Char Count: {stringToCount.Length}";
+            if (fromClip)
+            {
+                title += " (source - clipboard)";
+            }
+
+            var result = new Result
+            {
+                Score = 99,
+                IcoPath = "Images\\count.png",
+                Title = title,
+                SubTitle = cut
+            };
+            return result;
+        }
+
         public void Init(PluginInitContext context)
         {
         }
